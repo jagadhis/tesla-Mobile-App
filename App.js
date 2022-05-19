a704132b9cee58244b8e028ed77545fcb9bf0c2a
jagadhis <jwaran78@gmail.com>
2022-05-19 15:19:03 +0530

    completed the project

--- App.js
+++ App.js
@@ -2,11 +2,11 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, View } from "react-native";
 import CarsList from "./Components/CarList";
-
+import Header from "./Components/Header";
 export default function App() {
   return (
     <View style={styles.container}>
-      {/* <Header /> */}
+      <Header />
       <CarsList />
       <StatusBar style="auto" />
     </View>
